1e75440d1349804988d65c973c189a837d5734f1
dhanush199 <dhanushsh1995@gmail.com>
2019-05-06 19:28:25 +0530

    first commit

---
+++ src/main/java/com/bl/HwappApplication.java
@@ -0,0 +1,15 @@
+package com.bl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("AAAAA");
+		SpringApplication.run(HwappApplication.class, args);
+		System.out.println("ZZZZZ");
+	}
+
+}
